1e1effb7dd7defb61cdebf918cdc18af5ffb565f
carlos <guefe5@gmail.com>
2014-06-05 21:06:39 +0200

    Añadida BoxAndroidLibrary: compila y ejecuta

---
+++ BoxAndroidLibraryV2/build/source/buildConfig/debug/com/box/boxandroidlibv2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.box.boxandroidlibv2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.box.boxandroidlibv2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 304;
+  public static final String VERSION_NAME = "3.0.4-SNAPSHOT";
+}
